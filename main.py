161920af2a03c44cc31f132b7a50ea4b68c39e66
mohamed-mg1909062 <motheghoul@gmail.com>
2022-05-23 09:48:27 +0300

    Update

--- main.py
+++ main.py
@@ -7,20 +7,18 @@ import requests
 app = Flask(__name__)
 api = Api(app)
 
-# Define a list to store responses
-response = requests.get("https://api.github.com/search/repositories?q=stars:>0&sort=stars&order=desc&perpage=10/1").json()
-repositories = response["items"]
-top10 = repositories[:10]
+# Define a list to store response
+response = requests.get("https://api.github.com/search/repositories?q=stars:>0&sort=stars&order=desc&page=1&per_page=10").json()
+top10 = response["items"]
 
 # Create a resource
 class Repository(Resource):
     # GET method
     def get(self):
         return top10
-    
-# Add the resource to the API under a URL
-api.add_resource(Repository, "/top10")
 
+# Add the resource to the API under an appropriate route
+api.add_resource(Repository, "/repos/top10")
 
 # Run the server in debug mode
 if __name__ == "__main__":
